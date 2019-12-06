449e0fe2d79b14a3b6c0e5bfe813d2223d73c457
Mạc Đỗ Xuân Tính <mdxt13tk@gmail.com>
2019-12-06 22:02:25 +0700

    hide searchbox

--- app/src/main/java/com/moon/ailatrieuphu/api/APIConnect.java
+++ app/src/main/java/com/moon/ailatrieuphu/api/APIConnect.java
@@ -2,7 +2,7 @@ package com.moon.ailatrieuphu.api;
 
 public class APIConnect {
     //private static String baseURL="http://192.168.43.140:8081";
-    private static String baseURL="http://192.168.1.3:8081";
+    private static String baseURL="http://192.168.1.7:8081";
 
     public static APIService getServer(){
         return APIClient.getClient(baseURL).create(APIService.class);
