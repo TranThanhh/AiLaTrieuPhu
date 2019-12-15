c4e3813845f2b721665a57a36bcf81c5cdb70f18
Mạc Đỗ Xuân Tính <mdxt13tk@gmail.com>
2019-12-15 09:42:52 +0700

    Update APIService.java

--- app/src/main/java/com/moon/ailatrieuphu/api/APIService.java
+++ app/src/main/java/com/moon/ailatrieuphu/api/APIService.java
@@ -15,6 +15,15 @@ import retrofit2.http.Query;
 
 public interface APIService {
     //-----USER-----
+
+    //get list highscore
+    @GET("/users/high-score")
+    Call<List<User>> getAllPlayerHighScoreActive();
+
+    //get author's nickname for cauhoi.
+    @GET("/users/nickname")
+    Call<String> getNickname(@Query("idUser") int idUser);
+
     //check exists email, nickname
     @POST("/users-check")
     Call<String> checkUserExists(@Body User user);
@@ -39,52 +48,54 @@ public interface APIService {
     @PUT("/users/score")
     Call<String> modifyScore(@Body User user);
 
-    //get list highscore
-    @GET("/users/high-score")
-    Call<List<User>> getAllPlayerHighScoreActive();
-
-    //get author's nickname for cauhoi.
-    @GET("/users/nickname")
-    Call<String> getNickname(@Query("idUser") int idUser);
-
     //----------------------------------CAU HOI----------------------------------------------
     //GET List cau hoi theo idLoaiCH
     @GET("/cauhois/loai")
     Call<List<CauHoi>> getByIdLoaiCHActive(@Query("idLoaiCH") int idLoaiCH);
 
-    //-----------------------------------ADMIN----------------------------------------------
+    //-----------------------------------ADMIN CAUHOI----------------------------------------------
     //Get list all cauhoi.
     @GET("/admin/cauhois")
     Call<List<CauHoi>> getAllCauHoiActive();
-    //add new CauHoi
-    @POST("/admin/cauhois")
-    Call<String> addCauHoi(@Body CauHoi cauHoiNew);
-    // edit cauhoi.
-    @PUT("/admin/cauhois")
-    Call<String> updateCauHoi(@Body CauHoi cauHoiEdit);
-    //delete cauhoi.
-    @DELETE("/admin/cauhois")
-    Call<String> deleteCauHoi(@Query("idCauHoi") int idCauHoi);
+
     //search cauhoi.
     @GET("/admin/cauhois/filter")
     Call<List<CauHoi>> searchCauHoiActive(@Query("keyWord") String keyWord);
 
+    //add new CauHoi
+    @POST("/admin/cauhois")
+    Call<String> addCauHoi(@Body CauHoi cauHoiNew);
+
+    // edit cauhoi.
+    @PUT("/admin/cauhois")
+    Call<String> updateCauHoi(@Body CauHoi cauHoiEdit);
+
+    //delete cauhoi.
+    @DELETE("/admin/cauhois")
+    Call<String> deleteCauHoi(@Query("idCauHoi") int idCauHoi);
+
+    //-----------------------------------ADMIN USER----------------------------------------------
     //get list user.
     @GET("/admin/users-player")
     Call<List<User>> getAllPlayerActive();
+
     //get list moderator
     @GET("/admin/users-moderator")
     Call<List<User>> getAllModeratorActive();
+
     //search Player.
     @GET("/admin/users/filter-player")
     Call<List<User>> searchPlayerActive(@Query("keyWord") String keyWord);
-    //update role a user.
-    @PUT("/admin/users/role-level")
-    Call<String> updateRoleLevel(@Query("idUser") int idUser,@Query("roleLevel") int roleLevel, @Query("updateTime") String updateTime);
-    //delete a user.
-    @DELETE("/admin/users")
-    Call<String> deleteUser(@Query("idUser") int idUser);
+
     //cout cauhois of user.
     @POST("/admin/users/size-of-cauhoi")
     Call<Integer> countCauHoiOfUserActive(@Query("idUser") int idUser);
+
+    //update role a user.
+    @PUT("/admin/users/role-level")
+    Call<String> updateRoleLevel(@Query("idUser") int idUser, @Query("roleLevel") int roleLevel, @Query("updateTime") String updateTime);
+
+    //delete a user.
+    @DELETE("/admin/users")
+    Call<String> deleteUser(@Query("idUser") int idUser);
 }
