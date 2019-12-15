14f1b02a8846c261e8c55e06f61bd62ca9586565
thanhtran <tranthithanh200497@gmail.com>
2019-12-15 09:40:39 +0700

    Update APIConnect.java

--- app/src/main/java/com/moon/ailatrieuphu/api/APIConnect.java
+++ app/src/main/java/com/moon/ailatrieuphu/api/APIConnect.java
@@ -2,7 +2,7 @@ package com.moon.ailatrieuphu.api;
 
 public class APIConnect {
     //private static String baseURL="http://192.168.43.140:8081";
-    private static String baseURL="http://192.168.1.9:8081";
+    private static String baseURL="http://192.168.1.5:8081";
 
     public static APIService getServer(){
         return APIClient.getClient(baseURL).create(APIService.class);
